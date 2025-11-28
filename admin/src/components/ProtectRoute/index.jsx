e50ee2218ddb180e8a0301e2078ae1c84e2d4e01
Aditya Reddy <131836346+Aditya-799@users.noreply.github.com>
2025-11-29 01:19:41 +0530

    Redirect to Landing Page if user is not a recruiter

--- admin/src/components/ProtectRoute/index.jsx
+++ admin/src/components/ProtectRoute/index.jsx
@@ -5,7 +5,7 @@ const ProtectedRoute = ({ children }) => {
     const jwtToken = Cookies.get('recruiterToken')
     const decodedValue = jwtDecode(jwtToken)
     if (jwtToken === undefined || decodedValue.role !== 'recruiter') {
-        return <Navigate to={`${import.meta.env.VITE_REACT_APP_FRONTEND_URL}/login`} replace />
+        return <Navigate to={`${import.meta.env.VITE_REACT_APP_FRONTEND_URL}/`} replace />
     }
     return children
 }
